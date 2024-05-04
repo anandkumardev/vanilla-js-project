c888878e65c6d5c73865526dfcb9c13bc90e4673
Anand Kumar <93657634+anandkumardev@users.noreply.github.com>
2024-05-04 13:08:03 +0530

    Updated

--- responsive-sidebar-navigation/script.js
+++ responsive-sidebar-navigation/script.js
@@ -1,14 +1,26 @@
 const body = document.querySelector('body');
 const toggle = document.querySelector('.toggle');
 const sidebar = document.querySelector('.sidebar');
-const searchBtn = document.getElementById('.search-box');
-const modeSwitch = document.querySelectorAll('.toggle-switch');
+const searchBtn = document.querySelector('.search-box');
+const modeSwitch = document.querySelector('.toggle-switch');
 const modeText = document.querySelector('.mode-text')
 
 toggle.addEventListener('click', ()=> {
     sidebar.classList.toggle('close');
 });
 
+searchBtn.addEventListener('click', ()=> {
+    sidebar.classList.remove('close');
+});
+
 modeSwitch.addEventListener('click', ()=> {
     body.classList.toggle('dark');
+
+    if (body.classList.contains('dark')) {
+        modeText.innerText = "Light Mode";
+    } else {
+        modeText.innerText = "Dark Mode";
+    }
 });
+
+
