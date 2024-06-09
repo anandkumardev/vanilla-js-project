87350bef64106e7f859b82819383f1cc7093e20f
Anand Kumar <93657634+anandkumardev@users.noreply.github.com>
2024-06-09 22:59:27 +0530

    Updated

--- draggable-slider/script.js
+++ draggable-slider/script.js
@@ -5,7 +5,7 @@ arrowIcons = document.querySelectorAll(".wrapper i");
 let isDragStart = false, isDragging = false, prevPageX, prevScrollLeft, positionDiff;
 
 const showHideIcons = () => {
-    // showing and hiding prev/next icon according to carousel scroll left value
+    // showing & hiding prev/next icon according to carousel scroll left value
     let scrollWidth = carousel.scrollWidth - carousel.clientWidth; // getting the max scrollable width
     arrowIcons[0].style.display = carousel.scrollLeft == 0 ? "none" : "block";
     arrowIcons[1].style.display = carousel.scrollLeft == scrollWidth ? "none" : "block";
